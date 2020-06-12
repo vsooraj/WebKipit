92fe82de4b69852b143ae554e7436065cab94995
Sooraj <sooraj@ahbramtechnologies.com>
2020-06-12 18:12:42 +0530

    Add project files.

---
+++ WebKipit/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebKipit.Data;
+
+[assembly: HostingStartup(typeof(WebKipit.Areas.Identity.IdentityHostingStartup))]
+namespace WebKipit.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ WebKipit/Controllers/ReportsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebKipit.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Activeusers()
+        {
+            return View();
+        }
+        public IActionResult Newsignup()
+        {
+            return View();
+        }
+        public IActionResult SalesLiners()
+        {
+            return View();
+        }
+        public IActionResult SalesStickers()
+        {
+            return View();
+        }
+        public IActionResult InuseItems()
+        {
+            return View();
+        }
+
+        public IActionResult InuseLiners()
+        {
+            return View();
+        }
+        public IActionResult InuseStickers()
+        {
+            return View();
+        }
+        public IActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
